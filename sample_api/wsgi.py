c20481be3d34ef97ce9723ae62da46d1489045c4
lucashamaguchi <hamaguchi.lucas@gmail.com>
2020-04-18 01:31:43 -0300

    proxy pass

--- sample_api/wsgi.py
+++ sample_api/wsgi.py
@@ -1,5 +1,7 @@
 import os
 from dotenv import load_dotenv, find_dotenv
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(find_dotenv())
@@ -7,6 +9,8 @@ load_dotenv(find_dotenv())
 from .app import create_app  # noqa
 
 app = create_app()
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
 
 if __name__ == "__main__":
     app.run()
